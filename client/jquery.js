059f3e3bbe3f26f6602b93ca895d398a5e5dd052
umbone <henarivanaagit@gmail.com>
2020-03-09 16:41:29 +0700

    w

--- client/jquery.js
+++ client/jquery.js
@@ -5,6 +5,19 @@ let $loginForm = $('#login-form')
 
 let $hello = $('#hello')
 
+if (localStorage.getItem("token")) {
+    $(document).ready(() => {
+        $all.hide()
+        $hello.show()
+    })
+} else {
+    $(document).ready(() => {
+        $all.hide()
+        $login.show()
+        $hello.hide()
+    })
+}
+
 $loginForm.on('submit', e => {
     e.preventDefault()
     let $email = $('#email-login').val()
